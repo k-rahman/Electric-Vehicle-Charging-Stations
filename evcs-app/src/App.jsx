535031070dee21cf957b7b268d46061ce84a3a38
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-09-30 23:14:07 +0300

    Remove unecessary files

---
+++ evcs-app/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Map from './components/Map';
+import NavBar from './components/common/NavBar';
+import LoginForm from './components/LoginForm';
+import Register from './components/Register';
+
+function App() {
+
+   const LINKS = [
+         {name: 'Login', path: '/login'},
+         {name: 'register', path: '/register'}
+      ];
+
+  return (
+  <>
+      <NavBar siteName='EVCS' Links={LINKS}/>
+      <Route path='/' component={Map} />
+      <Route path='/login' component={LoginForm} />
+      <Route path='/Register' component={Register} />
+   </>
+  );
+}
+
+export default App;
