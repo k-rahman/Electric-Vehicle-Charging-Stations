1920f064654bd308b881b277dc766a1888bb5da8
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-09-30 23:05:46 +0300

    Add NavBar component

---
+++ evcs-app/src/components/common/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+const NavBar = ({ siteName = '', Links = [] }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <Link className="navbar-brand" to='/'>{siteName}</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+        {Links.map(link => (
+         <NavLink key={link.name} className="nav-link"
+            to={link.path}>{link.name}</NavLink>))}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
