40e494fa21587930fd131dec9b1e78d9429da852
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-10-03 10:46:13 +0300

    Add GeoCoder for location searching and styles to App.css

--- evcs-app/src/App.jsx
+++ evcs-app/src/App.jsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
 import Map from './components/Map';
 import NavBar from './components/common/NavBar';
 import LoginForm from './components/LoginForm';
 import Register from './components/Register';
+import './App.css';
 
-function App() {
+const API = process.env.REACT_APP_EVCS_API;
+
+const App = () => {
+   const [locations, setLocations] = useState([]);
    const LINKS = [
-         {name: 'Login', path: '/login'},
-         {name: 'register', path: '/register'}
-      ];
+      { name: 'Login', path: '/login' },
+      { name: 'register', path: '/register' }
+   ];
 
-  return (
-  <>
-      <NavBar siteName='EVCS' Links={LINKS}/>
-      <Route path='/' component={Map} />
-      <Route path='/login' component={LoginForm} />
-      <Route path='/Register' component={Register} />
-   </>
-  );
+   useEffect(() => {
+      console.log('mounted');
+      axios
+         .get(`${API}/api/locations`)
+         .then(res => {
+            setLocations(res.data);
+         })
+   }, []);
+
+   return (
+      <>
+         <NavBar siteName='EVCS' Links={LINKS} />
+         <Route path={['/locations/:id', '/']} render={routingProps =>
+            <Map locations={locations} {...routingProps} />} />
+         <Route path='/login' component={LoginForm} />
+         <Route path='/Register' component={Register} />
+         Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+      </>
+   );
 }
 
 export default App;
---
+++ evcs-app/src/components/GeoCoder.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import GlGeocoder from "react-map-gl-geocoder";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+
+
+const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+
+const GeoCoder = ({ mapRef, onViewportChange }) => {
+  return (
+    <GlGeocoder
+      mapRef={mapRef}
+      mapboxApiAccessToken={TOKEN}
+      onViewportChange={onViewportChange}
+      //localGeocoder={forwardGeocoder}
+      //render={handleRender}
+      countries="FI"
+      position="top-left"
+      reverseGeocode={true}
+      enableEventLogging={false}
+    />
+  );
+}
+
+export default GeoCoder;
