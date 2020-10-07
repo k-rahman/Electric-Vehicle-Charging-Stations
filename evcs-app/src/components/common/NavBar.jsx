16e6184a47dbb7099c2b6c9c65795ac4f7403f88
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-10-07 19:52:02 +0300

    replace NavLinks with buttons trying to keep user on the same url

--- evcs-app/src/components/common/NavBar.jsx
+++ evcs-app/src/components/common/NavBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const NavBar = ({ siteName = '', Links = [] }) => {
+const NavBar = ({ siteName = '', Links = [], onLinkClick }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#00959a' }}>
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <Link className="navbar-brand" to='/'>{siteName}</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -11,11 +11,11 @@ const NavBar = ({ siteName = '', Links = [] }) => {
       <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
         <div className="navbar-nav">
           {Links.map(link => (
-            <NavLink
+            <button
               key={link.name}
-              className='nav-link border-info border rounded-sm mx-1 text-white'
-              activeClassName='active-link'
-              to={link.path}>{link.name}</NavLink>))}
+              className='btn border-info border rounded-sm mx-1 text-white'
+              name={link.name}
+              onClick={onLinkClick}>{link.name}</button>))}
         </div>
       </div>
     </nav>
