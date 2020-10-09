536284eb12bb9d740618efe040c44b7a3de3e73d
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-10-09 21:47:45 +0300

    Renamed file

--- evcs-app/src/services/ouletService.js
+++
@@ -1,7 +0,0 @@
-import http from './httpService';
-
-const outletsAPI = process.env.REACT_APP_OUTLETS;
-
-export function getOutletsByStationId(stationId) {
-  return http.get(`${outletsAPI}/${stationId}`);
-};
