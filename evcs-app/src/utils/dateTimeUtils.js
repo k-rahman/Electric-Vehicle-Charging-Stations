8bf55bde7ced040bb04929a801f8920760690b98
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-10-09 21:46:48 +0300

    Write a util for time and date formate

---
+++ evcs-app/src/utils/dateTimeUtils.js
@@ -0,0 +1,10 @@
+export const convertTime = timeElapsed => ({
+     hrs: Math.floor((timeElapsed / (1000 * 60 * 60)) % 24),
+     mins: Math.floor((timeElapsed / 1000 / 60) % 60),
+     secs: Math.floor((timeElapsed / 1000) % 60),
+   })
+
+
+export const convertDateTime = dateTime => {
+  return new Date(dateTime.slice(0, 10).replace('T', ' ')).toLocaleDateString('FI');
+}
