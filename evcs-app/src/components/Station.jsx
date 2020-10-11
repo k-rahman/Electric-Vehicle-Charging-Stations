8580b6efc6d4eb1043ac930a2025e335d319101a
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-10-11 23:51:05 +0300

    Write station styles and structure

---
+++ evcs-app/src/components/Station.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { getOutletsByStationId } from '../services/outletService';
+import Outlet from './Outlet';
+import styles from '../assets/css/station.module.css';
+
+const Station = ({ station, stationNumber }) => {
+  const [outlets, setOutlets] = useState([]);
+
+  useEffect(() => {
+    const fetchOutlets = async () => {
+      const { data } = await getOutletsByStationId(station.id);
+      setOutlets(data);
+    }
+
+    fetchOutlets();
+  }, [station])
+
+  const countAvailable = () => {
+    let count = 0;
+    for (let outlet of outlets)
+      if (outlet.status === 'Available')
+        count++
+    return count > 0 ? count + ' outlet available' : 'All outlets are in use';
+  }
+
+  return (
+    <div className={`${styles['station-card']} card shadow-lg`}>
+      <div className={`${styles['station-header']} card-header py-1 pl-4`}>
+        <div className="row">
+          <div className="col">
+            <span>Station {stationNumber}</span>
+          </div>
+          <div className={`${styles['available-count']} col-7`}>
+            <span className="">
+              {countAvailable()}
+            </span>
+          </div>
+        </div>
+        <div className="row">
+          <div className= 'col'>
+            <small>{station.type} charging</small>
+          </div>
+        </div>
+      </div>
+      <ul className="list-group list-group-flush">
+        {outlets.map(outlet =>
+          <Outlet key={outlet.id} {...outlet} />)}
+      </ul>
+    </div>
+  );
+}
+
+export default Station;
