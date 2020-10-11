73aa4dc3b9092b3aea47bf6550904b415470d826
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-10-11 15:06:14 +0300

    Write final styles for monitor

--- evcs-app/src/components/common/Monitor.jsx
+++ evcs-app/src/components/common/Monitor.jsx
@@ -5,6 +5,7 @@ import { updateOutletStatus, getOutletById } from './../../services/outletServic
 import { convertTime } from '../../utils/dateTimeUtils';
 import { toast } from 'react-toastify';
 import Modal from './Modal';
+import styles from '../../assets/css/monitor.module.css';
 
 class Monitor extends Component {
   state = {
@@ -32,7 +33,7 @@ class Monitor extends Component {
     this.chargeTimeDate = new Date();
     this.startTime = Date.now();
     this.timer = setInterval(this.update, 1000);
-    this.setState({isRunning: true});
+    this.setState({ isRunning: true });
   }
 
   update = () => {
@@ -65,15 +66,12 @@ class Monitor extends Component {
 
   terminate = async () => {
     try {
-      const { data: success } = 
+      const { data: success } =
         await saveChargeInfo(this.DataToSave());
       if (success) {
         const { status } = await updateOutletStatus(this.props.outlet);
         if (status === 204) {
           this.setState({ showFinalResult: true });
-          const {data: outlet} = await getOutletById(this.props.outlet.id);
-          this.props.checkStatus(outlet);
-          console.log('check status called')
         }
       }
     }
@@ -106,21 +104,37 @@ class Monitor extends Component {
 
   chargingControls = () => {
     const { isRunning } = this.state;
-    let chargeButton = 
-    <button 
-      className='btn btn-danger' 
-      onClick={this.toggle}>{isRunning}StopCharging</button>
+
+    let stopChargingBtn =
+      <div className={`${styles.stop} row ml-2`}>
+        <div className="col-9">
+          <span >You can cancel the charging sequence by pressing the stop button.</span>
+        </div>
+        <div className="col">
+          <button
+            className={`${styles['stop-btn']} btn`}
+            onClick={this.toggle}>{isRunning}Stop charging</button>
+        </div>
+      </div>
 
     if (!isRunning) {
-      chargeButton =
+      stopChargingBtn =
         <>
-          <div>Are you sure you want to stop charging?</div>
-          <button className='btn btn-success' onClick={this.toggle}>No</button>
-          <button className='btn btn-danger' onClick={this.terminate}>Yes</button>
+          <div className={`${styles.confirm} row ml-2`}>
+            <div className="col-12">Are you sure you want to stop charging?</div>
+          </div>
+          <div className="row text-left ml-2">
+            <div className='col-3'>
+              <button className={`${styles['yes-btn']} btn`} onClick={this.terminate}>Yes</button>
+            </div>
+            <div className='col-3'>
+              <button className={`${styles['no-btn']} btn`} onClick={this.toggle}>No</button>
+            </div>
+          </div>
         </>
     }
 
-    return chargeButton;
+    return stopChargingBtn;
   }
 
   DataToSave = () => ({
@@ -138,16 +152,30 @@ class Monitor extends Component {
     if (!this.state.showFinalResult) {
       return (
         <>
-          <div>
-            <span>{this.addZero(2, hrs)} : </span>
-            <span>{this.addZero(2, mins)} : </span>
-            <span>{this.addZero(2, secs)}</span>
-          </div>
-          <div>
-            <span>cost {cost.toFixed(2)}</span>
-          </div>
-          <div>
-            <span>energy {energy.toFixed(2)}</span>
+          <div className="row mt-3">
+            <div className='row col-6 pl-4'>
+              <div className={`${styles['time-title']} col-12`}>Time elapsed (hh:mm:ss)</div>
+              <div className='w-100'></div>
+              <div className={`${styles.time} col-12`}>
+                <span>{this.addZero(2, hrs)} : </span>
+                <span>{this.addZero(2, mins)} : </span>
+                <span>{this.addZero(2, secs)}</span>
+              </div>
+            </div>
+            <div className='row col-3 text-center'>
+              <div className={`${styles.titles} col-12`}>Energy (kWh)</div>
+              <div className="w-100"></div>
+              <div className={`${styles.numbers} col-12`}>
+                {energy.toFixed(2)}
+              </div>
+            </div>
+            <div className='row col-4 text-right'>
+              <div className={`${styles.titles} col-6`}>Cost</div>
+              <div className="w-100"></div>
+              <div className={`${styles.numbers} col-12`}>
+                {cost.toFixed(2)} €
+              </div>
+            </div>
           </div>
           {this.chargingControls()}
         </>
@@ -156,16 +184,29 @@ class Monitor extends Component {
 
     return (
       <>
-        <div>Final cost {cost.toFixed(2)}</div>
-        <button
-          name='closeMonitor'
-          onClick={this.handleMonitorClose}>Close</button>
+        <div className='row text-center'>
+          <div className={`${styles.titles} col-12`}>Your charging session cost</div>
+          <div className="w-100"></div>
+          <div className={`${styles.numbers} col-12`}>
+            {cost.toFixed(2)} €
+              </div>
+        </div>
+        <div className="row text-center mt-4">
+          <div className='col-12'>
+            <button
+              name='closeMonitor'
+              className={`${styles['close-btn']} btn`}
+              onClick={this.handleMonitorClose}>Close</button>
+          </div>
+        </div>
+        <div className="row text-center mt-1">
+          <small className="col-12">Thank you for using our service. Remember to stay green!</small>
+        </div>
       </>
     )
   }
 
   handleMonitorClose = () => {
-
     $(this.modalRef.current).modal('hide');
     this.props.onMonitorClose();
   }
@@ -177,7 +218,9 @@ class Monitor extends Component {
         title='Charging screen'
         label='monitorModal'
         name='monitor'
-        showCloseButton={false}>
+        showCloseButton={false}
+        width='750px'
+      >
         {this.monitorState()}
       </Modal>
     );
